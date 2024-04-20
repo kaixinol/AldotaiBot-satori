70ed03a5af74aa6ea26ed43573fab99bc3ca9670
kaesinol <keiplyer@gmail.com>
2024-04-21 00:00:59 +0800

    :construction:bugfix

--- src/aldotaibot_satori/utils/setting.py
+++ src/aldotaibot_satori/utils/setting.py
@@ -1,19 +1,14 @@
-from dataclasses import field
-
 from pydantic.dataclasses import dataclass
-from typing import TypeVar
 from yaml import safe_load
-
-T = TypeVar('T')
 
 
 @dataclass
 class Config:
-    proxy: dict[str, str] | None = None
-    plugin: dict[str, T | dict] | None = None
-    token: str = ''
-    admin: list[int] = field(default_factory=list)
-    name: str = 'Aldotai'
+    proxy: dict[str, str]
+    plugin: dict[str, bool | dict]
+    admin: list[int]
+    auth_token: str
+    name: str
 
 
 config: Config | None = None
