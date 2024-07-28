825c7da3064e7e2b779ab17905ee829ab9298a09
kaesinol <keiplyer@gmail.com>
2024-07-28 23:03:26 +0800

    :bug:fix a serious bug

--- src/aldotaibot_satori/plugins/KeywordReply.py
+++ src/aldotaibot_satori/plugins/KeywordReply.py
@@ -6,7 +6,7 @@ from aldotaibot_satori.plugins import AUTHOR
 
 from ..utils.setting import config
 from satori import select, Author
-
+# from loguru import logger
 __plugin_metadata__ = PluginMetadata(author=AUTHOR, name=__name__)
 
 
@@ -15,6 +15,8 @@ async def _(event: MessageCreatedEvent, session: Session, msg: MessageChain):
     if event.quote and (authors := select(event.quote, Author)):
         if authors[0].id != event.account.self_id:
             return
+    if event.user.id == event.account.self_id or event.user is None:
+        return
     for regex, reply in config.plugin['KeywordReply']['regex']:
         if re.match(regex, event.message.content) and all([kw != msg.extract_plain_text() for kw, _ in
                                                            config.plugin['KeywordReply']['keywords']]):
